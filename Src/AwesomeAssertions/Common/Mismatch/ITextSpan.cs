bb69fddda41ea9b760dfb630a64291c6ef3cb8d4
* <tbui17@users.noreply.github.com>
2025-08-02 19:37:24 -0600

    c

---
+++ Src/AwesomeAssertions/Common/Mismatch/ITextSpan.cs
@@ -0,0 +1,16 @@
+﻿namespace AwesomeAssertions.Common.Mismatch;
+
+internal interface ITextSpan
+{
+    int Start { get; }
+
+    int End { get; }
+
+    string VisibleText { get; }
+
+    bool StartElided { get; }
+
+    bool EndElided { get; }
+
+    int Length { get; }
+}
--- Src/AwesomeAssertions/Common/Mismatch/TextSpan.cs
+++ Src/AwesomeAssertions/Common/Mismatch/TextSpan.cs
@@ -1,20 +1,5 @@
 ﻿namespace AwesomeAssertions.Common.Mismatch;
 
-internal interface ITextSpan
-{
-    int Start { get; }
-
-    int End { get; }
-
-    string VisibleText { get; }
-
-    bool StartElided { get; }
-
-    bool EndElided { get; }
-
-    int Length { get; }
-}
-
 internal record struct TextSpan
     : ITextSpan
 {
